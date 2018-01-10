df73efcbc59c8295ee8c2d864807dd859630f479
AdyFrancis <AdyFrancis@users.noreply.github.com>
2018-01-10 14:11:40 -0500

    Mailgrab Powershell Scripts
    Necessary for SDS Results Manager

---
+++ maildelete.ps1
@@ -0,0 +1,24 @@
+﻿$olFolderInbox = 6
+
+$outlook = new-object -com outlook.application;
+
+$ns = $outlook.GetNameSpace("MAPI");
+$inbox = $ns.GetDefaultFolder($olFolderInbox)
+$targetFolder = $inbox.Folders | Where-Object {$_.name -eq "SDSTest Results"}
+$subjects = Get-Content Z:\subjects.txt
+
+$count = 0
+$lines = $subjects.Count
+echo $lines
+while ($count -ne $lines) {
+    $targetFolder.items | foreach {
+        $subj = "$($_.Subject)"    
+        $contains = $subjects -contains $subj
+        if ($contains -eq $true) {
+            $count++
+            #$subjects | Where-Object {$_ -match $subj} | Set-Content .\newsubjects.txt
+            $($_).delete()
+        }
+    }
+}
+echo "$count emails deleted."
---
+++ mailgrab.ps1
@@ -0,0 +1,8 @@
+﻿$olFolderInbox = 6
+$outlook = new-object -com outlook.application;
+$ns = $outlook.GetNameSpace("MAPI");
+$inbox = $ns.GetDefaultFolder($olFolderInbox)
+$targetFolder = $inbox.Folders | Where-Object {$_.name -eq "SDSTest Results"}
+$targetFolder.items | foreach {
+    "$($_.Subject)" | out-file Z:\inbox.csv -Append
+}
